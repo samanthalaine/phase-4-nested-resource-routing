7466bb7325670c83a85e0924815b974dba394c96
Samantha Laine <samanthalaine8@gmail.com>
2021-09-22 17:31:43 -0400

    completed assignment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,18 @@
 class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
+# app/controllers/reviews_controller.rb
+
+def index
+  if params[:dog_house_id]
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+  else
     reviews = Review.all
-    render json: reviews, include: :dog_house
   end
+  render json: reviews, include: :dog_house
+end
+
 
   def show
     review = Review.find(params[:id])
